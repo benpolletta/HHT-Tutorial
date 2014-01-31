9277211109de661aa9bad7193fa0cb0498e8abd0
dangfq <dgibson@mit.edu>
2014-01-31 14:07:09 -0500

    added stupid comments
    stupid comments have been added

--- git_test_text_master.m
+++ git_test_text_master.m
@@ -3,3 +3,8 @@
 %Write your name here:
 
 %Lara
+%Dan - edited in Matlab, now attempting to commit to my own branch.
+%   Oh, I see.  That's impossible.  Having edited the version from the
+%   Master branch, I have to commit it back to the Master branch before I
+%   can even so much as *look* at another branch.  But we probably don't
+%   want to actually use branches anyway (point for discussion).
